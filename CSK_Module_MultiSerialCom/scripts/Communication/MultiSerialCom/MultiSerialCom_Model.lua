9168cac9de741c0f66e0be36947c36214d317a18
Roy Gollub <roy.gollub@sick.de>
2024-02-01 11:57:57 +0100

    - Initial commit of version 1.0.0

--- CSK_Module_MultiSerialCom/scripts/Communication/MultiSerialCom/MultiSerialCom_Model.lua
+++ CSK_Module_MultiSerialCom/scripts/Communication/MultiSerialCom/MultiSerialCom_Model.lua
@@ -31,13 +31,6 @@ function multiSerialCom.create(multiSerialComInstanceNo)
   self.multiSerialComInstanceNoString = tostring(self.multiSerialComInstanceNo) -- Number of this instance as string
   self.helperFuncs = require('Communication/MultiSerialCom/helper/funcs') -- Load helper functions
 
-  -- Optionally check if specific API was loaded via
-  --[[
-  if _G.availableAPIs.specific then
-  -- ... doSomething ...
-  end
-  ]]
-
   -- Create parameters etc. for this module instance
   self.activeInUI = false -- Check if this instance is currently active in UI
 
@@ -52,61 +45,73 @@ function multiSerialCom.create(multiSerialComInstanceNo)
   self.parametersName = 'CSK_MultiSerialCom_Parameter' .. self.multiSerialComInstanceNoString -- name of parameter dataset to be used for this module
   self.parameterLoadOnReboot = false -- Status if parameter dataset should be loaded on app/device reboot
 
-  --self.object = Image.create() -- Use any AppEngine CROWN
-  --self.counter = 1 -- Short docu of variable
-  --self.varA = 'value' -- Short docu of variable
+  self.serialComHandle = nil -- Handle of serial communication
+  self.open = false -- Status of serial connection
+  self.availableInterfaces = Engine.getEnumValues('SerialPorts') or '' -- Available SerialCom interfaces
+  self.availableInterfacesList = self.helperFuncs.createStringListBySimpleTable(self.availableInterfaces) -- List of available SerialCom interfaces
+
+  self.availableTypes = Engine.getEnumValues('SerialComTypes') or ''
 
   -- Parameters to be saved permanently if wanted
   self.parameters = {}
+  self.parameters.interface = self.availableInterfaces[1] -- Interface to use
   self.parameters.registeredEvent = '' -- If thread internal function should react on external event, define it here, e.g. 'CSK_OtherModule.OnNewInput'
   self.parameters.processingFile = 'CSK_MultiSerialCom_Processing' -- which file to use for processing (will be started in own thread)
-  --self.parameters.showImage = true -- Short docu of variable
-  --self.parameters.paramA = 'paramA' -- Short docu of variable
-  --self.parameters.paramB = 123 -- Short docu of variable
+  self.parameters.active = false -- Status if connection should be active
+  self.parameters.type = 'RS232' -- Type of serial interface, like 'RS232', 'RS422', 'RS485'
+  self.parameters.baudrate = 115200 -- Used baudrate, like 1200,2400,4800,9600,19200,38400,57600,115200
+  self.parameters.framingRxStart = 'None' -- Start framing for receiving, like 'None', 'STX', 'ETX', 'CR', 'LF', 'CR_LF'
+  self.parameters.framingRxStop = 'None' -- Stop framing for receiving
+  self.parameters.framingTxStart = 'None' -- Start framing for sending
+  self.parameters.framingTxStop = 'None' -- Stop framing for sending
 
-  self.parameters.internalObject = {} -- optionally
-  --self.parameters.selectedObject = 1 -- Which object is currently selected
-  --[[
-    for i = 1, 10 do
-    local obj = {}
+  self.parameters.framingBufferSizeRx = 10240 -- Maximum size of a packet which can be parsed by the received framing.
+  self.parameters.framingBufferSizeTx = 10240 -- Maximum size of a packet which can be parsed by the transmitted framing.
+  self.parameters.parity = 'N' -- Parity, like "N", "O", "E", "M", "S" (None, Odd, Even, Mark, Space)
+  self.parameters.handshake = false -- true to enable Xon/Xoff handshake, false to disable
 
-    obj.objectName = 'Object' .. tostring(i) -- name of the object
-    obj.active = false  -- is this object active
-    -- ...
+  self.parameters.receiveTimeout = 0 -- Timeout in ms to wait for received data. 0 is default and means directly return
+  self.parameters.receiveQueueSize = 10 -- Internal queue size for the receive()-function
+  self.parameters.discardIfFull = false -- Set to TRUE to discard the newest item which is currently added instead of discarding the oldest element
+  self.parameters.warnOverruns = true -- Set to false to disable warning on overruns when using the receive()-function
 
-    table.insert(self.parameters.internalObject, obj)
-  end
-
-  local internalObjectContainer = self.helperFuncs.convertTable2Container(self.parameters.internalObject)
-  ]]
+  self.parameters.dataBits = 8 -- Number of data bits (8 or 7)
+  self.parameters.flowControl = false -- Hardware flow control
+  self.parameters.stopBits = 1 -- Number of stop bits (1 or 2)
+  self.parameters.termination = false -- Internal termination
 
   -- Parameters to give to the processing script
   self.multiSerialComProcessingParams = Container.create()
   self.multiSerialComProcessingParams:add('multiSerialComInstanceNumber', multiSerialComInstanceNo, "INT")
   self.multiSerialComProcessingParams:add('registeredEvent', self.parameters.registeredEvent, "STRING")
-  --self.multiSerialComProcessingParams:add('showImage', self.parameters.showImage, "BOOL")
-  --self.multiSerialComProcessingParams:add('viewerId', 'multiSerialComViewer' .. self.multiSerialComInstanceNoString, "STRING")
 
-  --self.multiSerialComProcessingParams:add('internalObjects', internalObjectContainer, "OBJECT") -- optionally
-  --self.multiSerialComProcessingParams:add('selectedObject', self.parameters.selectedObject, "INT")
+  self.multiSerialComProcessingParams:add('active', self.parameters.active, "BOOL")
+  self.multiSerialComProcessingParams:add('interface', self.parameters.interface, "STRING")
+  self.multiSerialComProcessingParams:add('type', self.parameters.type, "STRING")
+  self.multiSerialComProcessingParams:add('baudrate', self.parameters.baudrate, "INT")
+  self.multiSerialComProcessingParams:add('framingRxStart', self.parameters.framingRxStart, "STRING")
+  self.multiSerialComProcessingParams:add('framingRxStop', self.parameters.framingRxStop, "STRING")
+  self.multiSerialComProcessingParams:add('framingTxStart', self.parameters.framingTxStart, "STRING")
+  self.multiSerialComProcessingParams:add('framingTxStop', self.parameters.framingTxStop, "STRING")
+  self.multiSerialComProcessingParams:add('framingBufferSizeRx', self.parameters.framingBufferSizeRx, "INT")
+  self.multiSerialComProcessingParams:add('framingBufferSizeTx', self.parameters.framingBufferSizeTx, "INT")
+  self.multiSerialComProcessingParams:add('parity', self.parameters.parity, "STRING")
+  self.multiSerialComProcessingParams:add('handshake', self.parameters.handshake, "BOOL")
+  self.multiSerialComProcessingParams:add('receiveTimeout', self.parameters.receiveTimeout, "INT")
+  self.multiSerialComProcessingParams:add('receiveQueueSize', self.parameters.receiveQueueSize, "INT")
+  self.multiSerialComProcessingParams:add('discardIfFull', self.parameters.discardIfFull, "BOOL")
+  self.multiSerialComProcessingParams:add('warnOverruns', self.parameters.warnOverruns, "BOOL")
+
+  self.multiSerialComProcessingParams:add('dataBits', self.parameters.dataBits, "INT")
+  self.multiSerialComProcessingParams:add('flowControl', self.parameters.flowControl, "BOOL")
+  self.multiSerialComProcessingParams:add('stopBits', self.parameters.stopBits, "INT")
+  self.multiSerialComProcessingParams:add('termination', self.parameters.termination, "BOOL")
 
   -- Handle processing
   Script.startScript(self.parameters.processingFile, self.multiSerialComProcessingParams)
 
   return self
 end
-
---[[
---- Some internal code docu for local used function to do something
-function multiSerialCom:doSomething()
-  self.object:doSomething()
-end
-
---- Some internal code docu for local used function to do something else
-function multiSerialCom:doSomethingElse()
-  self:doSomething() --> access internal function
-end
-]]
 
 return multiSerialCom
 
--- README.md
+++ README.md
@@ -1,27 +1,20 @@
 # CSK_Module_MultiSerialCom
-## INFO: Draft version. Not further developed / tested so far! Check for available GitHub forks of this repository to see latest updates.
 
-Module / Application to provide [...] functionality.
+Module to provide serial communication functionality.  
 
-*If available, please also add a screenshot/gif of the UI of the module here placed within /docu/media/ (see code)*
 ![](./docu/media/UI_Screenshot.png)
 
 ## How to Run
-
-[***...please fill with informations...***]  
-For further information check out the [documentation](https://raw.githack.com/SICKAppSpaceCodingStarterKit/CSK_Module_MultiSerialCom/main/docu/CSK_Module_MultiSerialCom.html) in the folder "docu".
+The app includes an intuitive GUI to setup the database.  
+For further information check out the [documentation](https://raw.githack.com/SICKAppSpaceCodingStarterKit/CSK_Module_MultiSerialCom/main/docu/CSK_Module_MultiSerialCom.html) in the folder "docu".  
 
 ## Information
 
 Tested on:
 |Device|Firmware|Module version
 |--|--|--|
-|e.g. SICK AppEngine|V1.5.0|V0.1.0|
-
-[***optionally***]
-Following CSK modules are used for this application via Git subtrees and should NOT be further developed within this repository (see [contribution guideline](https://github.com/SICKAppSpaceCodingStarterKit/.github/blob/main/Contribution_Guideline.md) of this GitHub organization):  
-
-  * CSK_Module_XYZ (release/tag v1.2.3)
+|SICK AppEngine|V1.5.0|V1.0.0|
+|SIM1012|V2.3.0|V1.0.0|
 
 This application / module is part of the SICK AppSpace Coding Starter Kit developing approach.  
 It is programmed in an object-oriented way. Some of the modules use kind of "classes" in Lua to make it possible to reuse code / classes in other projects.  
